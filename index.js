11b2b887fe6599b1d6b84fe21bd5a8e3ad4fbafb
Carlos Ramirez <carlosramirez686@gmail.com>
2021-12-31 12:18:51 -0800

    Assignment Completed

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 31
+let num2 = 2
+const multiply = num1 * num2
 
-// Write your code here
+var random = Math.floor(Math.random() * 10);
+
+let num3 = 16
+let num4 = 4
+const mod = num3 / num4
+
+const max = Math.max(1,2,3,4,5,6,20)
